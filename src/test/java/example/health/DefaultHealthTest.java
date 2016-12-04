e7b9c3043607e67a6ad1a8a9a0dfe47a9c1a6595
Mike Winter <mwinter@thoughtworks.com>
2016-12-04 14:25:02 +0000

    Added Hello World endpoint.

--- src/test/java/example/health/DefaultHealthTest.java
+++ src/test/java/example/health/DefaultHealthTest.java
@@ -1,4 +1,4 @@
-package example;
+package example.health;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Test;
@@ -9,15 +9,14 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static helpers.matchers.HealthCheckMatcher.healthy;
-import static helpers.matchers.JsonNodeMatcher.missing;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {"spring.profiles.active=production"})
-@RunWith(SpringRunner.class)
 @SuppressWarnings("SpringJavaAutowiredMembersInspection")
-public class ProductionHealthTest {
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class DefaultHealthTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
@@ -30,10 +29,11 @@ public class ProductionHealthTest {
     }
 
     @Test
-    public void shouldNotIncludeDiskSpaceMetricsInUnauthenticatedRequests() throws Exception {
-        JsonNode status = restTemplate
-                .getForObject("/health", JsonNode.class);
+    public void shouldIncludeDiskSpaceMetrics() throws Exception {
+        JsonNode disk = restTemplate
+                .getForObject("/health", JsonNode.class)
+                .with("diskSpace");
 
-        assertThat(status, is(missing("diskSpace")));
+        assertThat(disk, is(healthy()));
     }
 }
--- src/test/java/example/hello/HelloTest.java
+++ src/test/java/example/hello/HelloTest.java
@@ -1,6 +1,5 @@
-package example;
+package example.hello;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static helpers.matchers.HealthCheckMatcher.healthy;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -16,24 +14,15 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @RunWith(SpringRunner.class)
 @SuppressWarnings("SpringJavaAutowiredMembersInspection")
-public class DefaultHealthTest {
+public class HelloTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void shouldBeHealthyAfterLaunch() throws Exception {
-        JsonNode status = restTemplate
-                .getForObject("/health", JsonNode.class);
+    public void shouldReturnHelloWorldWhenNoNameIsGiven() throws Exception {
+        String message = restTemplate
+                .getForObject("/hello", String.class);
 
-        assertThat(status, is(healthy()));
-    }
-
-    @Test
-    public void shouldIncludeDiskSpaceMetrics() throws Exception {
-        JsonNode disk = restTemplate
-                .getForObject("/health", JsonNode.class)
-                .with("diskSpace");
-
-        assertThat(disk, is(healthy()));
+        assertThat(message, is("Hello World!"));
     }
 }
